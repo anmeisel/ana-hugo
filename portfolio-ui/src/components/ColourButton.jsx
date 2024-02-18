60a49a82cffe10331375c8be42597b6847f71031
Ana Meisel <anameisel@Anas-MacBook-Air.local>
2024-02-18 13:04:45 +0000

    no portfolio

--- portfolio-ui/src/components/ColourButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./ColourButtonStyle.css"
-
-export default function ColourButton(props) {
-    return (
-        <button 
-        onClick={() => props.action(props.col)} style={{backgroundColor: props.col}} className="btn">
-        </button>
-    )
-}
